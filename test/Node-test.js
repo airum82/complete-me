45f730fb0784a2eab1295c60a83c18d33dc2877b
Arram Mandel <airum82@gmail.com>
2018-05-15 15:40:23 -0600

    Reformatted node children and trie insertions to be objects instead of arrays, currently reformatting tests

---
+++ test/Node-test.js
@@ -0,0 +1,50 @@
+import Node from '../lib/Node.js';
+import { assert } from 'chai';
+
+describe.only('Node', () => {
+  let node;
+  beforeEach(() => {
+    node = new Node('a');
+  })
+  it('should have a data property', () => {
+    assert.equal(node.data, 'a');
+  })
+  it('should have a parent property', () => {
+    assert.equal(node.parent, null);
+  })
+  it('should have a children property', () => {
+    assert.deepEqual(node.children, {});
+  })
+  describe('#addChild', () => {
+    it('should have an add child method', () => {
+      assert.isFunction(node.addChild);
+    })
+    it('should add a child', () => {
+      let childNode = new Node('p');
+      node.addChild(childNode);
+      assert.deepEqual(node.children, { p: childNode });
+      assert.deepEqual(childNode.parent, node)
+    })
+    it('should not add childNode with duplicate data', () => {
+      let childNode = new Node('p');
+      let childNode2 = new Node('p')
+      node.addChild(childNode);
+      assert.deepEqual(node.children, { p: childNode });
+      node.addChild(childNode2);
+      assert.deepEqual(node.children, { p: childNode });
+    })
+    it('should not add childNode with duplicate data', () => {
+      let childNode = new Node('p');
+      let childNode2 = new Node('p')
+      node.addChild(childNode);
+      assert.deepEqual(node.children, [childNode]);
+      node.addChild(childNode2);
+      assert.deepEqual(node.children, [childNode]);
+    })
+  })
+  // it('should take one argument and insert in a given place', () => {
+  //   let node = new Node('a')
+  //   trie.insert(node);
+  //   assert.deepEqual(trie.head, 'a');
+  // })
+});
--- test/Trie-test.js
+++ test/Trie-test.js
@@ -1,7 +1,8 @@
 import Trie from '../lib/Trie.js';
 import { assert } from 'chai';
 
-describe.only('test', () => {
+
+describe('test', () => {
   let trie;
   beforeEach(() => {
     trie = new Trie();
@@ -9,13 +10,40 @@ describe.only('test', () => {
   it('should have an insert method', () => {
     assert.isFunction(trie.insert)
   })
-  it('should take one argument and insert in a given place', () => {
-    trie.insert('pizza');
+  it('should have a count method', () => {
+    assert.isFunction(trie.count)
+  })
+  it('should start off with a count of 1 for the root node', () => {
     assert.deepEqual(trie.count(), 1);
   })
-  it('should increment the count upon each insertion', () => {
+  it('should take one argument and create a new node for each letter the first time', () => {
     trie.insert('pizza');
-    trie.insert('steak');
-    assert.deepEqual(trie.count(), 2);
+    assert.deepEqual(trie.count(), 6);
+  })
+  it('trie.root should contain new node as child', () => {
+    assert.deepEqual(trie.root, Node { data: null, parent: null, children: {} });
+    trie.insert('pizza');
+    assert.deepEqual(trie.count(), 6);
+    assert.deepEqual(trie.root.p, 'p');
+  })
+  it('should increment the count upon each insertion', () => {
+    trie.insert('word');
+    trie.insert('would');
+    assert.deepEqual(trie.root.children.data, ['w']);
+    // assert.deepEqual(trie.root.children, ['w']);
+  })
+  it('should insert new node with prefix matching data as child of prefix node', () => {
+    trie.insert('piz');
+    trie.insert('pizza');
+    assert.deepEqual(trie.insertions[0].children[0].data, 'pizza');
+  })
+  it('should have an suggest method', () => {
+    assert.isFunction(trie.suggest)
+  })
+  it.skip('should suggest possible words based on letter(s) entered', () => {
+    trie.insert('pizza');
+    trie.insert('pizzeria');
+    trie.insert('apple');
+    assert.equal(trie.suggest('piz'), ['pizza', 'pizzeria'])
   })
 });
